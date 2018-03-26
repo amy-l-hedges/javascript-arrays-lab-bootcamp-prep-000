851f614031c459564cefacacc4103991e398d5d0
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-03-26 23:48:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destrctivelyAppendKitten(name) {
-  return array.push()
+  return kittens.push()
 }
 
 function destructivelyPrependKitten(name) {
